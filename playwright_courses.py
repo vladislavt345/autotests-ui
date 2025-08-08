baf14cadd63fc129c53c49b03768112d8d682562
vladislavt345 <vladislavt30515@gmail.com>
2025-08-08 19:34:15 +0300

    assert > expect

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,46 +1,44 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    page.wait_for_timeout(2000)
 
-    email_input = page.locator('[data-testid="registration-form-email-input"] input')
-    email_input.fill("user.name@gmail.com")
-    
-    username_input = page.locator('[data-testid="registration-form-username-input"] input')
-    username_input.fill("username")
-    
-    password_input = page.locator('[data-testid="registration-form-password-input"] input')
-    password_input.fill("password")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-    page.wait_for_timeout(2000)
-    registration_button = page.locator('[data-testid="registration-page-registration-button"]')
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
-    page.wait_for_timeout(2000)
 
     context.storage_state(path="browser-state.json")
 
-    context.close()
-    browser.close()
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-    page.wait_for_timeout(2000)
 
-    courses_title = page.locator('[data-testid="courses-list-toolbar-title-text"]')
-    assert courses_title.inner_text() == "Courses"
-    
-    empty_state_title = page.locator('[data-testid="courses-list-empty-view-title-text"]')
-    assert empty_state_title.inner_text() == "There is no results"
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-    icon = page.locator('[data-testid="courses-list-empty-view-icon"]')
-    assert icon.is_visible(), "Empty state icon should be visible"
+    empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
 
-    context.close()
-    browser.close()
+    empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
